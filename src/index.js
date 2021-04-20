912a9cef7d70cf4befe4f1afe2a33b4f5123121c
Scooter <scott.skrobel@gmail.com>
2021-04-20 11:14:18 -0600

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const cookieRecipie = {
   chocolateChips: 4,
 };
 
+const cookieBaseRecipie = {
+  ...cookieRecipie,
+  dough: 1,
+  chocolateChips: 4,
+}
+
 console.log(cookieRecipie);
+console.log(cookieBaseRecipie);
